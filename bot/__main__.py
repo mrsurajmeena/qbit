d6ee0b394150a8afd1ed26989d4548373839ac71
mrsurajmeena <64419292+mrsurajmeena@users.noreply.github.com>
2021-04-23 12:47:34 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -16,7 +16,9 @@ from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_tim
 from .helper.telegram_helper.filters import CustomFilters
 from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, anime, stickers, search, delete, speedtest, usage
 
-now=datetime.now(pytz.timezone('Asia/Jakarta'))
+from datetime import datetime
+
+now=datetime.now(pytz.timezone('Asia/kolkata'))
 
 @run_async
 def stats(update, context):
